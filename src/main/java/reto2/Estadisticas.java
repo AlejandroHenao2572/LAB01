ad3722402affca8eabb42a992485d61c018dda42
AlejandroHenao2572 <david.patacon-h@mail.escuelaing.edu.co>
2025-08-29 13:00:22 -0500

    Initial commit

--- src/main/java/reto2/Estadisticas.java
+++ src/main/java/reto2/Estadisticas.java
@@ -17,7 +17,7 @@ public class Estadisticas {
     public int getCantidad() { return cantidad; }
 
     public static Estadisticas calcularConCarreraParalela(List<Integer> lista) {
-        int max = CarreraParalela.maximo(lista);
+        int max = CarreraParalela.maximo(lista);   
         int min = CarreraParalela.minimo(lista).get(0);
         int cantidad = CarreraParalela.minimo(lista).get(1);
         return new Estadisticas(max, min, cantidad);
