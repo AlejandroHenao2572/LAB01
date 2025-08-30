ce007f03c3afc158a36c6a216dd0a1394ef2a82a
BoneBiter255 <felix2295772@gmail.com>
2025-08-30 00:49:41 -0500

    funcion repetir e invertir

--- src/main/java/reto3/reto3.java
+++ src/main/java/reto3/reto3.java
@@ -1,6 +1,6 @@
 package main.java.reto3;
 
-public class MensajeBuilder {
+public class reto3 {
     public static String repetirMensaje(String mensaje) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 3; i++) {
@@ -10,12 +10,21 @@ public class MensajeBuilder {
         return sb.toString();
     }
 
-    public static String invertirMensajeConBuffer(String mensaje) {
+    public static String invertirMensaje(String mensaje) {
         return new StringBuffer(mensaje).reverse().toString();
+    }
+
+public static String repetirEInvertir(String mensaje) {
+        String mensajeRepetido = repetirMensaje(mensaje);
+        return invertirMensaje(mensajeRepetido);
     }
 
     public static void main(String[] args) {
         String resultado = repetirMensaje("Hola mundo");
-        System.out.println(resultado); 
+        String resultado2 = invertirMensaje("Hola mundo");
+        String resultado3 = repetirEInvertir("Hola mundo");
+        System.out.println("Repetir mensaje: " + resultado);
+        System.out.println("Invertir mensaje: " + resultado2);
+        System.out.println("Repetir 3 veces e invertir: " + resultado3); 
     }
 }
