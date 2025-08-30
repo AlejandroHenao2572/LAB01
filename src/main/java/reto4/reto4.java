194c6f213d317050d1e2830b1447b62f3b8dd25b
AlejandroHenao2572 <david.patacon-h@mail.escuelaing.edu.co>
2025-08-30 01:27:03 -0500

    Funcion con HashMap

--- src/main/java/reto4/reto4.java
+++ src/main/java/reto4/reto4.java
@@ -1,5 +1,31 @@
 package main.java.reto4;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class reto4 {
     
+    public static HashMap<String, Integer> almacenarEnHashMap(List<ParClaveValor> pares) {
+        HashMap<String, Integer> mapa = new HashMap<>();
+        for (ParClaveValor par : pares) {
+            if (!mapa.containsKey(par.clave)) {
+                mapa.put(par.clave, par.valor);
+            }
+        }
+        return mapa;
+    }
+    
+    public static void main(String[] args) {
+        List<ParClaveValor> pares = List.of(
+            new ParClaveValor("a", 1),
+            new ParClaveValor("b", 2),
+            new ParClaveValor("a", 3), // Esta clave duplicada sera ignorada
+            new ParClaveValor("c", 4),
+            new ParClaveValor("b", 5)  // Esta clave duplicada será ignorada
+        );
+        
+        HashMap<String, Integer> resultado = almacenarEnHashMap(pares);
+        System.out.println("HashMap resultante: " + resultado);
+    }
+
 }
