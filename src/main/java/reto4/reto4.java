76c0527e4d7241b709a93967f17754d59263d5f6
AlejandroHenao2572 <david.patacon-h@mail.escuelaing.edu.co>
2025-08-30 02:08:51 -0500

    Funcion para imprimir en mayusculas

--- src/main/java/reto4/reto4.java
+++ src/main/java/reto4/reto4.java
@@ -41,13 +41,26 @@ public class reto4 {
         return mapaCombinadoHashMap;
     }
     
+    public static void imprimirConClavesMayusculas(HashMap<String, Integer> mapa, String nombreMapa) {
+        System.out.print(nombreMapa + ": {");
+        boolean primero = true;
+        for (Map.Entry<String, Integer> entry : mapa.entrySet()) {
+            if (!primero) {
+                System.out.print(", ");
+            }
+            System.out.print(entry.getKey().toUpperCase() + "=" + entry.getValue());
+            primero = false;
+        }
+        System.out.println("}");
+    }
+    
     public static void main(String[] args) {
         List<ParClaveValor> pares = List.of(
             new ParClaveValor("a", 1),
             new ParClaveValor("b", 2),
             new ParClaveValor("a", 3), // Esta clave duplicada sera ignorada
             new ParClaveValor("c", 4),
-            new ParClaveValor("b", 5)  // Esta clave duplicada será ignorada
+            new ParClaveValor("b", 5)  // Esta clave duplicada sera ignorada
         );
         
         HashMap<String, Integer> resultado = almacenarEnHashMap(pares);
@@ -57,5 +70,7 @@ public class reto4 {
         System.out.println("HashMap resultante: " + resultado);
         System.out.println("Hashtable resultante: " + resultado2);
         System.out.println("Mapa combinado: " + mapaCombinadoHashMap);
+        System.out.println("Impresion con claves en mayusculas");
+        imprimirConClavesMayusculas(resultado, "HashMap Mayusculas");
     }
 }
